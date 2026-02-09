38c2b8845534f86a7d10a9368070b01f032aaae7
Farhod <fsoyilov@gmail.com>
2026-02-10 01:55:27 +0500

    fix: revert to BrowserRouter and original vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // ...
 
-<HashRouter>
+<BrowserRouter>
   <Routes>
     <Route path="/" element={<Index />} />
     <Route path="/docs" element={<Docs />} />
@@ -24,7 +24,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
     {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
     <Route path="*" element={<NotFound />} />
   </Routes>
-</HashRouter>
+</BrowserRouter>
         </TooltipProvider >
       </GlobalErrorBoundary >
     </ThemeProvider >
